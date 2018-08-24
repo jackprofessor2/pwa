4d0cf3cf926f3864b1e74c861f79cb969a84f0da
JonasDiego21 <jonasdiegorodriguesmartins@yahoo.com.br>
2018-08-23 21:47:33 -0300

    Atualização scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,12 @@
 /** scripts.js */
-/*
-button.addEventListener('pointerup', function(event) {
-    navigator.bluetooth.requestDevice({ filters: [{ services: ['battery_service'] }] })
-    .then(device => device.gatt.connect())
-    .then(server => {
-    // Getting Battery Service...
-    return server.getPrimaryService('battery_service');
-    })
-    .then(service => {
-    // Getting Battery Level Characteristic...
-    return service.getCharacteristic('battery_level');
-    })
-    .then(characteristic => {
-    // Reading Battery Level...
-    return characteristic.readValue();
-    })
-    .then(value => {
-    console.log('Battery percentage is ' + value.getUint8(0));
-    })
-    .catch(error => { console.log(error); });
-});
-*/
-
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-      navigator.serviceWorker.register('sw.js').then(function(registration) {
-        // Registration was successful
-
-        var button = document.getElementById('bluetooth');
-
-        button.addEventListener('click', function(r) {
-            window.alert('Evento disparado', r.responseText);
+        navigator.serviceWorker.register('sw.js').then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }).catch(function(err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
         });
-
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      }).catch(function(err) {
-        // registration failed :(
-        console.log('ServiceWorker registration failed: ', err);
-      });
     });
 }
--- sw.js
+++ sw.js
@@ -73,3 +73,24 @@ self.addEventListener('activate', function(event) {
       })
     );
 });
+
+button.addEventListener('pointerup', function(event) {
+    navigator.bluetooth.requestDevice({ filters: [{ services: ['battery_service'] }] })
+    .then(device => device.gatt.connect())
+    .then(server => {
+    // Getting Battery Service...
+    return server.getPrimaryService('battery_service');
+    })
+    .then(service => {
+    // Getting Battery Level Characteristic...
+    return service.getCharacteristic('battery_level');
+    })
+    .then(characteristic => {
+    // Reading Battery Level...
+    return characteristic.readValue();
+    })
+    .then(value => {
+    console.log('Battery percentage is ' + value.getUint8(0));
+    })
+    .catch(error => { console.log(error); });
+});
