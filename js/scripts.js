65bdfbf59302313c25147610bd65b5067f25cb71
JonasDiego21 <jonasdiegorodriguesmartins@yahoo.com.br>
2018-08-23 22:15:51 -0300

    Atualização sw.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,8 @@ if ('serviceWorker' in navigator) {
         navigator.serviceWorker.register('sw.js').then(function(registration) {
             // Registration was successful
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
-            registration.update();
+            
+            console.log(button);
         }).catch(function(err) {
             // registration failed :(
             console.log('ServiceWorker registration failed: ', err);
