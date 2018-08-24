31dbf2570d6cdb0e60f4396ba55c9adeea4cec41
JonasDiego21 <jonasdiegorodriguesmartins@yahoo.com.br>
2018-08-23 22:03:03 -0300

    Atualização sw.js

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,7 @@ if ('serviceWorker' in navigator) {
 }
 */
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js', {scope: 'sw-test'})
+    navigator.serviceWorker.register('sw.js')
     .then(function(registration) {
         // registration worked
         console.log('Registration succeeded.');
@@ -22,7 +22,7 @@ if ('serviceWorker' in navigator) {
             registration.update();
         }    
     }).catch(function(error) {
-            // registration failed
-            console.log('Registration failed with ' + error);
+        // registration failed
+        console.log('Registration failed with ' + error);
     });
 };
